115d34280b1ea9d6dbcc3c98317d9d5227f7013e
Antek Piechnik <antek.piechnik@gmail.com>
2014-01-17 04:13:49 +0100

    Adds support for time-series.json api

--- lib/moneda/rates.rb
+++ lib/moneda/rates.rb
@@ -12,6 +12,10 @@ module Moneda
       parse_response fetch_historical_on(date)
     end
 
+    def time_series(start_date, end_date)
+      parse_response fetch_time_series(start_date, end_date)
+    end
+
     private
 
     def app_id
@@ -20,6 +24,10 @@ module Moneda
 
     def fetch_historical_on(date)
       HTTParty.get("#{Moneda::API_URL}/historical/#{date}.json?app_id=#{app_id}&base=#{base_currency}").body
+    end
+
+    def fetch_time_series(from, to)
+      HTTParty.get("#{Moneda::API_URL}/api/time-series.json?app_id=#{app_id}&base=#{base_currency}&start=#{from}&end=#{to}").body
     end
 
     def parse_response(response)
